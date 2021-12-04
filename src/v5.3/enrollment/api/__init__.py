2c8beea188d8dcb58f14bb164a714cb50be1a185
Marcos Alcozer <marcos@alcozer.dev>
2021-12-04 20:35:30 +0000

    Adds enrollment SDK

---
+++ src/v5.3/enrollment/api/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.local_education_agencies_api import LocalEducationAgenciesApi
+from swagger_client.api.schools_api import SchoolsApi
+from swagger_client.api.sections_api import SectionsApi
+from swagger_client.api.staffs_api import StaffsApi
+from swagger_client.api.students_api import StudentsApi
